4dcc205e5cfc42d255bfea39db1384803f87b8b8
JuanfranZZ <55857618+JuanfranZZ@users.noreply.github.com>
2021-12-04 21:01:18 +0100

    Update main.ino
    Simplified and fixed code

--- main.ino
+++ main.ino
@@ -1,36 +1,37 @@
+
 //Library
 #include "SevenSeg.h"
 
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
-int count = 0; // number of pushing the button
+const int relePin = 12;     // Rele variables
+const int dotPin = 4;
 
 //Defines the segments A-G: SevenSeg(A, B, C, D, E, F, G);
 SevenSeg disp (5,6,7,8,9,10,11);
-//Number of 7 segments
-const int numOfDigits = 1;
-//CC(or CA) pins of segment
-int digitPins [numOfDigits]={4};
+const int numOfDigits = 1;  //Number of 7 segments
+int digitPins [numOfDigits]={4};  //CC(or CA) pins of segment
 
 //Variables
-int number=0; //Default number
-int flag;
+int _number = 0; //Default number
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int _buttonState = 0;         // variable for reading the pushbutton status
 
 //timer variables
-int delayStart = 0;
-long int timing = 0;
-const int dotPin = 4;
-int delayFin = 3000;
-bool finish = true;
-bool starting = false;
-const long int timeScale = 600000; // 10min each unit
+int _count = 0; // number of pushing the button
+
+long int _timing = 0;
+long int _timingButton = 0;
+long int _initButtonTiming = 0;
+
+// Contant that won't change
+const long int _timeScale_ = 600000; // 10min each unit
+const int _blinkTimer_ = 500; // timing for dot blinking
+long int _delayReadingButton_ = 2000;
 
 
-// Rele variables
-const int relePin = 12;
+
 
 // SET UP --------------------------------------------------------------
 
@@ -49,9 +50,6 @@ void setup() {
   disp.setDutyCycle(50);
   disp.setDPPin(dotPin);
 
-  //Timer
-  delayStart = millis();
-
   //Rele
   pinMode(relePin, OUTPUT);
   releON(false);
@@ -61,90 +59,25 @@ void setup() {
 // ----------------------------------------------------------- END SET UP
 
 
-void showLED(int number){
-  //Valid range is from 0 to 9
-  if (number>=0 && number<=9){
-    //Print number to 7 segment display
-    disp.writeDigit(number);
-      //Print message to serial monitor only once
-      if (flag==0){ 
-        flag=1;
-      }
+void updateLED(int n){
+  if (n>=0 && n<=9){  //Valid range is from 0 to 9
+    disp.writeDigit(n);  //Print number to 7 segment display
   }
   else {
     disp.clearDisp();
   }
 }
 
-void resetTiming(){
-  delayStart = millis();
-}
-
 bool isButtonPressed(){
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH){
-    resetTiming();
+  _buttonState = digitalRead(buttonPin);
+  if (_buttonState == HIGH){
     disp.clearDP();
     return true;
   }
   return false;
 }
 
-
-void buttonCounter(){
-  Serial.print("Setting number:");
-  Serial.println(count);
-  if (count == 10){
-    count = 0;
-  }
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (isButtonPressed()) {
-    count = count + 1;
-    delay(200);
-  }
-}
-
-
-bool isDelayFin(int delaySelected){
-  timing = millis() - delayStart;
-  if (timing > delaySelected){
-    return true;
-  }
-  return false;
-}
-
-
-bool runTimer(int numbering){
-  int stepTime = 50;
-  bool light = true;
-  long int t0 = 0;
-  long int timer = 0;
-
-  timer = numbering * timeScale;
-  Serial.print("timeScale:");
-  Serial.println(timeScale);
-  delay(1000);
-  t0 = timer;
-  while (!isButtonPressed() && timer >= 0){
-    if ((t0-timer)/500 > 0){
-      t0 = timer;
-      light = blinkLED(light);
-    }
-    Serial.println(timer);
-    if (timer/1000 == 0){
-      Serial.println("exit");   
-      delay(1000);   
-      return true;
-    }
-    delay(stepTime);
-    count = timer/timeScale;
-    showLED(count);
-    timer = timer - stepTime;
-  }
-  Serial.println("Button pressed");
-  return true;
-}
 
 bool blinkLED(bool up){
   if(up){
@@ -168,25 +101,68 @@ void releON(bool s){
 }
 
 
+void buttonWatcher(){
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (isButtonPressed()) {
+    Serial.println("BUTTON PRESSED---------------------------------------");
+    _initButtonTiming = millis();
+    _count = _count + 1;
+    updateLED(_count);
+    if (_count == 10){
+      _count = 0;
+      updateLED(_count);
+    }
+    Serial.print("Count:"); Serial.println(_count);
+    delay(200);
+    while(_timingButton < _delayReadingButton_){
+      buttonWatcher();
+    }
+    _timingButton = 0;
+  }
+  _timingButton = millis() - _initButtonTiming;
+}
+
+bool runTimer(){
+  int stepTime = 10;
+  bool light = true;
+  long int t0 = 0;
+  long int timer = 0;
+
+  timer = _count * _timeScale_;
+  t0 = timer;
+  while (timer >= 0){
+    if ((t0-timer)/_blinkTimer_ > 0){
+      t0 = timer;
+      light = blinkLED(light);
+    }
+    if (isButtonPressed()){
+      Serial.println("Button pressed, exit from loop");
+      buttonWatcher();
+      break;
+    }
+    delay(stepTime);
+    _count = timer/_timeScale_;
+    updateLED(_count);
+    timer = timer - stepTime;
+  }
+  Serial.println("runTimer ended");
+  return true;
+}
+
 void loop()
 {
-  Serial.println("Starting...");
-  count = -1;
-  starting = false;
-  while (!isDelayFin(delayFin) or starting == false){
-    buttonCounter();
-    showLED(count);
-    starting = true;
-  }
-  while (isDelayFin(delayFin) && (count >= 0) && starting){
-    finish = false;
-    starting = true;
-    while (!finish){
-      releON(true);
-      finish = runTimer(count);
-    }
-    starting = false;
-    releON(false);
-  }
+  _count = -1;
+  Serial.println("Init loop");
+  Serial.print("Count: "); Serial.println(_count);
 
+  buttonWatcher();
+
+  if (_count > 0){
+    releON(true);
+    runTimer();
+  }
+  releON(false);
+  _timingButton = 0;
+  disp.clearDisp();
+  Serial.println("End");
 }
